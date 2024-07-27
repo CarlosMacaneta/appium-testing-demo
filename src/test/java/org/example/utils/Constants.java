fe70856e20adf5428a167e2b845e50cad5bb7491
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-07-27 13:41:22 +0200

    feat: adds tests constants

---
+++ src/test/java/org/example/utils/Constants.java
@@ -0,0 +1,11 @@
+package org.example.utils;
+
+
+public class Constants {
+    public static String APPIUM_PATH = "C:\\Users\\dell\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
+    public static String APPIUM_SERVER = "http://192.168.58.121:4723";
+    public static String APPIUM_IP_ADDRESS = "192.168.58.121";
+    public static int APPIUM_IP_ADDRESS_PORT = 4723;
+    public static String DEVICE_NAME =  "samsung SM-A235F";
+    public static String APK_PATH = "C:\\Users\\dell\\Documents\\projects\\personal\\SchoolContentManagerComposer\\app\\build\\outputs\\apk\\debug\\app-debug.apk";
+}
