c5c6ad7e236b7fc77a9d759de3933b60bf4fc0ed
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-07-27 13:40:54 +0200

    feat: implementing Appium auto start and stop

--- src/test/java/org/example/AppAppiumTest.java
+++ src/test/java/org/example/AppAppiumTest.java
@@ -2,8 +2,12 @@ package org.example;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.example.utils.Constants;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -11,17 +15,31 @@ import java.net.URL;
 
 public class AppAppiumTest {
 
+    private AppiumDriverLocalService appiumServiceBuilder() {
+        return new AppiumServiceBuilder()
+                .withAppiumJS(new File(Constants.APPIUM_PATH))
+                .withIPAddress(Constants.APPIUM_IP_ADDRESS)
+                .usingPort(Constants.APPIUM_IP_ADDRESS_PORT)
+                .build();
+    }
+
+    @SuppressWarnings("resource")
     @Test
     public void setup() throws MalformedURLException, URISyntaxException {
 
-        URL serverUrl = new URI("http://127.0.0.1:4723/").toURL();
+        URL serverUrl = new URI(Constants.APPIUM_SERVER).toURL();
+
+        //starting appium automatically
+        appiumServiceBuilder().start();
 
         // Getting device details for automation
         UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName("your_device_name")
-                .setApp("your_apk");
+        options.setDeviceName(Constants.DEVICE_NAME)
+                .setApp(Constants.APK_PATH);
 
         AndroidDriver androidDriver = new AndroidDriver(serverUrl, options);
         androidDriver.quit();
+
+        appiumServiceBuilder().stop();
     }
 }
